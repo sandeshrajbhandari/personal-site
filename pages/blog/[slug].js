df071554658e631391ddb6c556358c5d6d750efb
tubuntu2 <sandeshrb49@gmail.com>
2022-05-13 16:06:06 +0545

    /blog url lists articles like the homepage. changed basic logic and layouts.

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -17,7 +17,7 @@ export default function Post({ title, date, content }) {
       date={new Date(date).toISOString()}
       type="article"
     >
-      <article className="flex flex-col items-start justify-center w-full max-w-2xl mx-auto mb-16">
+      <article className="flex flex-col items-start justify-center w-full customMaxWidth mx-auto mb-16">
         <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
           {title}
         </h1>
@@ -62,7 +62,7 @@ export async function getStaticProps(context) {
     //props: blogPosts.find((item) => item.slug===params.slug), // will be passed to the page component as props
     props: {
       ...data,
-      date: data.date.toISOString(),
+      date: data.date,
       content: mdxSource
     }
   };
